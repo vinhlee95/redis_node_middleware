bff937dbcf75aa9628f4f06eaa18d22173e3233d
Vinh Le <vinh@swappie.com>
2021-11-24 07:34:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`App is running in port ${PORT}`)
+})
+
+
+app.get('/stores', (req, res) => {
+    const stores = [
+        {id: 1, name: 'Apple'},
+        {id: 1, name: 'Samsung'},
+    ]
+
+    const foo = 'bar'
+
+    res.status(200).json(stores)
+})
